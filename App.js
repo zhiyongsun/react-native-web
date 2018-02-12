e784f14070a66e44cb2bd1d973d51ad4f1ad46d4
sunzhiyong036 <785037725@qq.com>
2018-02-12 17:37:38 +0800

    change text

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ const instructions = Platform.select({
     'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' +
     'Shake or press menu button for dev menu',
+  web: "Hello World!"
 });
 
 type Props = {};
